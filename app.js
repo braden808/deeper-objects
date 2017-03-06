3c376ac69ac3bedcff9c492fcb384d91e0c9f836
Braden Amorozo <braden808@gmail.com>
2017-03-06 08:43:18 -1000

    update

--- app.js
+++ app.js
@@ -4,22 +4,32 @@
 */
 function accessObjectInArray(arr){
 	//Print the parameter being passed through
-
+//console.log(arr);
 	//How can we tell what type of data this is?
 	//By it's notation, the top level is bracket notation, therefore the parameter is an array
 
 	//What methods can we use for this item?
 	//Array methods
-
+//console.log(arr.length);
 	//Access and print the first object in the parameter 
-
+//console.log(arr[0]);
 	//Access and print the third object in the parameter
-
+//console.log(arr[2]);
 	//Iterate through the array to print all the objects in the parameter
-
+for (var i = 0; i < arr.length; i++) {
+	//console.log(arr[i])
+	//console.log("inner objects");
+	//console.log(arr[i]);
+	for (var key in arr){
+	//console.log(key);
+	console.log(arr[key]);
+}
+}
 		//Can you concatanete a string to an object?
-
+//NO
 		//Iterate through the key:value pairings and print all the keys and their values
+
+
 
 	//Clean up your code by creating variable names and setting their values
 }
@@ -48,12 +58,29 @@ console.log("\n*** End ***\n")
 */
 function accessArrayInObject(obj){
 	//Is the parameter an array or an object?
-
+console.log(obj);
 	//Print the paramter being passed through the function
 
 	//Iterate through the object
-
+for (var key in obj){
+	console.log("key");
+	console.log(key);
+	console.log("\nvalues\n");
+	console.log(obj[key]);
+	console.log("\n");
 	//Create a conditional to check if the key is "cars"
+	if (key === "cars"){
+		console.log("Inside conditional for 'cars'")
+		console.log(obj[key]);
+		for (var carValues in "cars"){
+			console.log("Values for key 'cars'")
+			console.log(obj[key][carValues]);
+		}
+	}
+}
+
+
+
 
 	//Iterate through and print the values in the key "cars"
 
